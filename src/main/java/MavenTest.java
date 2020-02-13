c977ee1ba9d57e1ad46a587b4f82f2953c09a0ad
Hung Ly <hunghly@gmail.com>
2020-02-13 12:06:19 -0600

    finished maven project

---
+++ src/main/java/MavenTest.java
@@ -0,0 +1,20 @@
+
+import org.apache.commons.lang3.*;
+
+import java.util.Scanner;
+
+
+public class MavenTest {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter something.");
+        String str = s.nextLine();
+        if (StringUtils.isNumericSpace(str)) {
+            System.out.printf("%s is numeric!\n", str);
+        } else {
+            System.out.printf("%s is not numeric or has a '-' sign in front.\n", str);
+        }
+        System.out.printf("Your string with flipped case is: %s.\n", StringUtils.swapCase(str));
+        System.out.printf("Your string reversed is: %s.\n", StringUtils.reverse(str));
+    }
+}
